605df006c64718673dd611a5699c62bf0876857e
SGK01 <borison2@naver.com>
2026-09-03 12:33:15 +0900

    fix: Speed 변수명을 MoveSpeed로 수정

--- Assets/02.Scripts/Enemy.cs
+++ Assets/02.Scripts/Enemy.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float Speed = 1.0f;
+    public float MoveSpeed = 1.0f;
 
     private void Update()
     {
         Vector2 direction = Vector2.down;
-        transform.Translate(direction * Speed * Time.deltaTime);
+        transform.Translate(direction * MoveSpeed * Time.deltaTime);
     }
 }
