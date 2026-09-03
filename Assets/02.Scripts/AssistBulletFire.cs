43d9ba81e2d168e33b35b3dba64b91dd75dbc77a
SGK01 <borison2@naver.com>
2026-09-03 13:21:30 +0900

    feat: 보조 총알에 파괴 기능과 데미지 다르게 구현

--- Assets/02.Scripts/AssistBulletFire.cs
+++ Assets/02.Scripts/AssistBulletFire.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 public class AssistBulletFire : MonoBehaviour
 {
     // 목적: 총알을 위로 움직이고 싶다.
     public float Speed = 3.0f;
+    public float AssistBulletDamage = 1.0f;
+
     private void Update()
     {
         Fire();
@@ -14,5 +17,19 @@ public class AssistBulletFire : MonoBehaviour
         Vector2 Direction = Vector2.up;
         transform.Translate(Direction * Speed * Time.deltaTime);
     }
-}
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            enemy.Health -= AssistBulletDamage;
+            if (enemy.Health <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
