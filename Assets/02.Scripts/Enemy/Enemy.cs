3a84ec7c9623b5a1f2d4ae196ecd283c51778ca2
SGK01 <borison2@naver.com>
2026-09-04 14:51:27 +0900

    fix: 플레이어가 NULL 값일시 데미지가 안들어가고 오류메시지 나오게 수정

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -29,6 +29,11 @@ public abstract class Enemy : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player")) return;
         Player player = collision.gameObject.GetComponent<Player>();
+        if (player == null)
+        {
+            Debug.LogWarning("플레이어가 NULL 입니다.");
+            return;
+        }
         player.TakeDamage(_damage);
         Destroy(gameObject);
     }
