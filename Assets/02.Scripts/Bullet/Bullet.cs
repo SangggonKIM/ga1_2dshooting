c45803adc5428f4254a693a817bd7129ea182957
SGK01 <borison2@naver.com>
2026-09-09 17:15:42 +0900

    feat: 총알 소리 pitch가 쏠때마다 다르게 수정

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private AudioSource _audioSource;
     // 목적: 총알을 위로 움직이고 싶다.
     public float Speed = 1.0f;
     public float BulletDamage = 1.0f;
 
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.pitch = UnityEngine.Random.Range(1f, 1.5f);
+        _audioSource.Play();
+    }
     private void Update()
     {
         Fire();
