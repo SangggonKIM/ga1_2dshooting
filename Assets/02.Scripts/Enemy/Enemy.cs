9dbb13b9b15c8c522a69afc1f9ffdd810f502687
SGK01 <borison2@naver.com>
2026-09-09 15:46:07 +0900

    feat: 적 공격당할때 사운드추가, 아이템 획득 사운드 추가

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -9,18 +9,16 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected float _moveSpeed = 1.0f;
     private int _animHit = Animator.StringToHash("hitTrigger");
     private Animator _animator;
+    // Todo: 에너미가 공격 당할때 재생시켜주는 피격 사운드
+    private AudioSource _damagedaudioSource;
     // - 죽을때 생성할 파티클 프리팹
     [SerializeField] private GameObject _deathEffectPrefab;
     private void Awake()
     {
         // 애니메이터 컴포넌트에 대한 참조를 가져와서 할당한다.
         _animator = GetComponent<Animator>();
+        _damagedaudioSource = GetComponent<AudioSource>();
     }
-
-    private void Start()
-    {
-    }
-
     private void Update()
     {
         MoveAction();
@@ -33,6 +31,7 @@ public abstract class Enemy : MonoBehaviour
     {
         _animator.SetTrigger(_animHit);
         _health -= damage;
+        _damagedaudioSource.Play();
         if (_health <= 0)
         {
             // 너죽자!
